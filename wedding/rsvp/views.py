20a90c0f403aeef53e5904ebba8a8e8853dc2366
Andy <chryso@gmail.com>
2013-12-27 14:44:06 -0500

    Starting to flesh out content

--- wedding/rsvp/views.py
+++ wedding/rsvp/views.py
@@ -4,14 +4,27 @@ from django.shortcuts import render
 
 # Create your views here.
 def rsvp_ajax(req):
-    
+
     email = 'boo'
-    
-    if req.method == 'POST':        
+
+    if req.method == 'POST':
         email = req.POST['email']
         if '@' in email:
             ret = 'success %s' % email
         else:
             ret = 'failure'
-    
+
     return HttpResponse(ret)
+
+def rsvp_submit_ajax(req):
+
+	if req.method != 'POST':
+		return HttpResponse('failure')
+
+	guestlist = []
+
+	for name, value in req.POST:
+		if name.startswith('guests'):
+			guestlist.append(req.POST[name])
+
+	return HttpResponse('acknowledged the following guests: %s' % guestlist)
