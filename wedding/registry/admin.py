1ac4d06751222c686fca0c4107e1951fbcff4b4a
Andy <chryso@gmail.com>
2014-05-19 16:28:46 -0400

    Polished up the registry section, added a link field

--- wedding/registry/admin.py
+++ wedding/registry/admin.py
@@ -4,16 +4,16 @@ from registry.models import Activity, Giftor
 
 class GiftorInline(admin.TabularInline):
     model = Giftor
-    
+
     list_display = ('guest', 'num_bought','paid',)
     readonly_fields = ('email',)
-    
-    
+
+
 class ActivityAdmin(admin.ModelAdmin):
     model = Activity
-    list_display = ('name', 'unit_price', 'num_units','blurb',)
+    list_display = ('name', 'unit_price', 'num_units','blurb','link',)
     readonly_fields = ('total_cash', 'remaining_cash', 'remaining_units',)
-    
+
     inlines = [GiftorInline,]
 
 admin.site.register(Activity, ActivityAdmin)
--- wedding/registry/models.py
+++ wedding/registry/models.py
@@ -10,31 +10,32 @@ class Activity(models.Model):
     unit_price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
     num_units = models.IntegerField(default=1)
     blurb = models.CharField(max_length=1024, default="no blurb yet!")
-    
+    link = models.URLField()
+
     def total_cash(self):
         return self.unit_price * self.num_units
-    
+
     def remaining_cash(self):
         return self.total_cash() - self.cash_received()
-    
+
     def cash_received(self):
         return self.total_cash() - self.remaining_units() * self.unit_price
-    
+
     def remaining_units(self):
         total_bought = self.giftor_set.all().aggregate(Sum('num_bought'))['num_bought__sum'] or 0
         return self.num_units - total_bought
-    
+
     def __unicode__(self):
         return self.name
-    
+
 class Giftor(models.Model):
     activity = models.ForeignKey(Activity)
     guest = models.ForeignKey('rsvp.Guest')
     num_bought = models.IntegerField(max_length=1024, default=0)
     paid = models.BooleanField(default=False)
-    
+
     def email(self):
         return self.guest.email
-    
+
     def __unicode__(self):
         return '%s: %s (%s)' % (self.activity, self.guest, self.num_bought)
