ab2098e6ef8f0468ee1de52b6525ba8b909f8b30
Andy <chryso@gmail.com>
2014-05-19 18:08:18 -0400

    Adding to the registry

--- wedding/registry/urls.py
+++ wedding/registry/urls.py
@@ -5,5 +5,6 @@ import registry.views
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', registry.views.ActivityListView.as_view(), name='main'),
-    # url(r'^blog/', include('blog.urls')),    
+    url(r'^pledge/', registry.views.PledgeView.as_view(), name='pledge'),
+    # url(r'^blog/', include('blog.urls')),
 )
--- wedding/registry/views.py
+++ wedding/registry/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views.generic import View
+from django.contrib import messages
 
 from .models import Activity
 
 
 class ActivityListView(View):
-    template_name = 'registry/main.html'    
+    template_name = 'registry/main.html'
     model = Activity
-    
+
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'activities': Activity.objects.all()})
+        return render(request, self.template_name, {'activities': self.model.objects.all()})
+
+class PledgeView(View):
+	summary_template_name = 'registry/pledge_summary.html'
+
+	def post(self, request, *args, **kwargs):
+		pledges = self.build_pledges(request)
+		grand_total = sum([t[2] for t in pledges])
+		return render(request, self.summary_template_name, {'pledges': pledges, 'grand_total': grand_total})
+
+	def build_pledges(self, request):
+		ret = []
+
+		for k in request.POST:
+			if k.startswith('activity_id_'):
+				activity = Activity.objects.get(pk=request.POST[k])
+				amount = int(request.POST.get('amount_%s' % k.split('_')[-1], "0") or "0")
+				total = amount * activity.unit_price
+				if amount:
+					ret.append((activity,amount,total))
+
+		return ret
+
+
